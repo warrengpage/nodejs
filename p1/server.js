a67f908af33974adf317bf39cfa5c42eee7e0a78
Warren Page <warren@warrenpage.com>
2017-11-21 20:17:40 +1100

    update with example

--- p1/server.js
+++ p1/server.js
@@ -1,6 +1,18 @@
 var http = require('http')
-http.createServer(function( req, res) {
+
+var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var server = http.createServer(function( req, res) {
+    console.log('request!!');
     res.writeHead(200, {'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(8080, "0.0.0.0");
-console.log('Server running at http://0.0.0.0:8080/');
+    
+})
+
+console.log('Initializing Server on ' + ip + ':' + port);
+
+server.listen(port,ip, function(){
+	var address = server.address();
+	console.log('Server running on ' + address.address + ':' + address.port);
+});
